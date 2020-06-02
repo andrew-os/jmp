53c06b80421335e2bd55763517336d09cb71f3ea
Andrew Sleigh <andrewsleigh@akauk.com>
2020-06-02 03:36:35 +0100

    nav logo component create and widths and heights of logo at dt and mobile

---
+++ src/components/NavLogo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavLogo.css'
+
+export default () => (
+    <div 
+    className="Logo Logo--Nav"
+    style={{
+        backgroundImage: `url(/images/JM-Black.svg)`
+    }}
+    >
+    </div>
+)
