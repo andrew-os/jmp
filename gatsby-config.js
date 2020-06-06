49b178cd7c2a1884b2a87260633763e35a9ae01a
Andrew Sleigh <andrewsleigh@akauk.com>
2020-06-06 03:15:03 +0100

    add styled componets yarn, amend slider markup in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
+    `gatsby-plugin-emotion`,
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
