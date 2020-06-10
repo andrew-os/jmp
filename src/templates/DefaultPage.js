1e1ecccb6b614c5e22c47ca65c0c100844d7860f
Andrew Sleigh <andrewsleigh@akauk.com>
2020-06-10 06:30:06 +0100

    bg Styled component for pages contact and about

--- src/templates/DefaultPage.js
+++ src/templates/DefaultPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styled from '@emotion/styled'
 
-import PageHeader from '../components/PageHeader'
+
 import Content from '../components/Content'
 import Layout from '../components/Layout'
 import SVGIcon from '../components/SVGIcon'
@@ -9,21 +10,37 @@ import SVGIcon from '../components/SVGIcon'
 import './DefaultPage.css'
 
 // Export Template for use in CMS preview
+const ClipBG = styled.div`
+background: ${props => `url(${props.background}) no-repeat top center`};
+background-size: cover;
+`
 export const DefaultPageTemplate = ({
   title,
   featuredImage,
   body
 }) => (
   <main className="DefaultPage">
-    <PageHeader
-      backgroundImage={featuredImage}
-    />
-
     <section className="section">
-      <div className="container">
-        <h1>{title}</h1>
-        <Content source={body} />
-        <SVGIcon src="/images/calendar.svg" />
+      <div className="clip-content">
+        <ClipBG className="clip-cut" background={featuredImage}>
+          <div className="bg-w"></div>
+          <div className="inner-cont">
+            <div className="inner-cont-w">
+              <div className="container">
+                <div className="row row-align-btween">
+                  <div className="lg:gr-5 gr-12">
+                    <header>
+                      <h1>{title}</h1>
+                    </header>
+                    <Content source={body} />
+                    <SVGIcon src="/images/calendar.svg" />
+                  </div>
+                  <div className="lg:gr-4 gr-12"></div>
+                </div>  
+              </div>
+            </div>
+          </div>
+          </ClipBG>
       </div>
     </section>
   </main>
